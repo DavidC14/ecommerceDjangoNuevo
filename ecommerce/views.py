020024610ffaf3e1608dfd8c8b18db76322c9277
DavidC14 <chajade.david.et36@gmail.com>
2022-11-01 22:01:19 -0300

    1.0

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,5 @@
+import re
+from unittest import result
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
@@ -5,11 +7,106 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+from django.http import JsonResponse
 from .models import stockProducts, carrito
 from . import forms
 from ecommerce import models
-import operator
+from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
+import operator, sys
+from paypalcheckoutsdk.orders import OrdersGetRequest , OrdersCaptureRequest
+
+
 # Create your views here.
+
+def pago(request):
+    pass
+
+class PayPalClient:
+    def __init__(self):
+        self.client_id = "AVuJsj71tA1V4cwBpLWi1t8KSlZVZ6xy3RLOwWcHPYqoZR7UYT4M24II899EoqDKOmSnV4GN53OvuauP"
+        self.client_secret = "EKpyYcKu54VJ5nlhQqOD1rJv60o4e2rh0BJo_MPqShr7H1kTO2lHPfP96tSOCvM4jaJ5alcnxWtkidzn"
+        
+        self.environment = SandboxEnvironment(client_id=self.client_id, client_secret=self.client_secret)
+
+        self.client = PayPalHttpClient(self.environment)
+
+    def object_to_json(self, json_data):
+        result = {}
+        if sys.version_info[0] < 3:
+            itr = json_data.__dict__.iteritems()
+        else:
+            itr = json_data.__dict__.items()
+        for key,value in itr:
+            if key.startswith("__"):
+                continue
+            result[key] = self.array_to_json_arrray(value) if isinstance(value, list) else\
+                        self.object_to_json(value) if not self.is_primittive(value) else\
+                            value
+        return result
+
+    def array_to_json_array(self, json_array):
+        result=[]
+        if isinstance(json_array, list):
+            for item in json_array:
+                result.append(self.object_to_json(item) if not self.is_primittive(item) \
+                                else self.array_to_json_array(item) if isinstance(item, list) else item)
+        return result
+
+    def is_primittive(self, data):
+        return isinstance(data, str) or isinstance(data, unicode) or isinstance(data, int)
+
+class GetOrder(PayPalClient):
+
+  def get_order(self, order_id):
+    request = OrdersGetRequest(order_id)
+
+    response = self.client.execute(request)
+    # print('Status Code: ', response.status_code)                    
+    # print('Status: ', response.result.status)
+    # print ('Order ID: ', response. result.id)              
+    # print ('Intent: ', response. result. intent)                  
+    # print ('Links:')
+                                                   
+    # for link in response.result.links:
+    #   print('\t{}: {}\tCall Type: {}'.format(link.rel, link.href, link.method))
+    # print('Gross Amount: {} {}'.format(response.result.purchase_units[0].amount.currency_code,
+    #                     response.result.purchase_units[0].amount.value))
+
+if __name__ == '__main__':
+    GetOrder().get_order('REPLACE-WITH-VALID-ORDER-ID')
+
+
+class CaptureOrder (PayPalClient):
+  
+   def capture_order(self, order_id, debug=False):
+     request = OrdersCaptureRequest(order_id)
+
+     response = self.client.execute(request)
+     if debug:
+       print('Status Code: ', response.status_code)
+       print('Status: ', response.result.status)
+       print('Order ID: ', response.result.id)
+       print('Links: ')
+       for link in response.result.links:
+            print('\t{}: {}\tCall Type: {}'.format(link.href, link.method))
+       print('Capture Ids: ')
+       for purchase_unit in response.result.purchase_units:
+            for capture in purchase_unit.payments.captures:
+                print('\t', capture.id)
+            print("Buyer:")
+            # print ("\tEmail Address: {}\n\tName: {}\n\tPhone Number: {}".format(response.result.payer.email_address,
+            #     response.result.payer.name.given_name + " " + response.result.payer.name.surname,
+            #     response.result.payer.phone.phone_number.national_number))
+            return response
+
+if __name__ == "__main__":
+    order_id = 'REPLACE-WITH-APPORVED-ORDER-ID'
+    CaptureOrder().capture_order(order_id, debug=True)
+
+
+
+def hola(request):
+    return render(request, 'paypal.html')
 
 def home(request):
     return render(request, 'home.html')
@@ -131,8 +228,13 @@ def addCart(request, prod):
     
     return redirect('cart')
 
+
+
 def contacto(request):
     
+    
+
+
     if request.method=="POST":
         
         miFormulario=forms.FormularioContacto(request.POST)
@@ -146,6 +248,8 @@ def contacto(request):
     else:
 
         miFormulario=forms.FormularioContacto()
+
+    
     
     return render(request, "formulario_contacto.html", {"form":miFormulario})
 
